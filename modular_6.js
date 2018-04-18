8cac9c39a4d637cafed1e0b6d72b1d19cd000c08
Sannu <smahat.official@gmail.com>
2018-04-18 17:01:27 +0300

    Modular on progress

--- modular_6.js
+++ modular_6.js
@@ -1,4 +1,23 @@
 var dir = process.argv[2];
 var ext = process.argv[3];
 
-module.exports = function(dir, ext) {};
+fs.readdir(dir, function callback(err, list) {
+    //fs.readdir reads the files in a directory.
+    if (err) {
+      console.log("There's an error");
+    }
+  
+    for (var i = 0; i < list.length; i++) {
+      var array = path.extname(list[i]); //here, path.extname method gets the extension name of the file starting from '.'
+  
+      if (array === ext) {
+        console.log(list[i]);
+      }
+    }
+  });
+
+  data = is filtered list of files
+
+module.exports = function(dir, ext,(err,data)=>{if (err) {
+    console.log("There's an error");
+  }}) {};
